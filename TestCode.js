485226a5294150efbcefd4d15b935deb044ef5c1
Jan-Ivar Abrahamsen <jan-ivar.abrahamsen@webstep.no>
2026-01-19 14:52:15 +0000

    Test kode

---
+++ TestCode.js
@@ -0,0 +1,27 @@
+// Declare variables
+let adjective = "mystical";
+let noun = "dragon";
+let verb = "sleeping";
+let place = "enchanted forest";
+let adjective2 = "giant";
+let noun2 = "marshmallows";
+
+// Create first story
+let firstStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+// Output first story
+console.log("First story: " + firstStory);
+
+// Assign new values
+adjective = "tiny";
+noun = "robot";
+verb = "dancing";
+place = "futuristic city";
+adjective2 = "sparkling";
+noun2 = "batteries";
+
+// Create second story
+let secondStory = `Once upon a time, there was a(n) ${adjective} ${noun} who loved to eat ${noun2}. The ${noun} lived in a ${place} and had ${adjective2} nostrils that blew fire when it was ${verb}.`;
+
+// Output second story
+console.log("Second story: " + secondStory);
